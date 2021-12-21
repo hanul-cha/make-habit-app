166ae1e2abdeb9963f9e03e3aa27ead966cafab6
hanul-cha <ckgks3014@naver.com>
2021-12-21 18:22:04 +0900

    해결했다!!!

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,13 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+
 const login = (req: any, res:NextApiResponse) => {
     if(req.method === "POST") {
         const params = req.body.params
-        res.setHeader("Set-Cookie",`params=1234;Max-Age=3600;HttpOnly,Secure`)
+        console.log(params.name)
+        res.setHeader(`Set-Cookie`,`params=${params.id};Max-Age=3600;HttpOnly,Secure`)
         res.statusCode = 200;
-        res.json({ message: 'ok' });
-        console.log(params)
+        res.json({ message: params });
     }
+    /* 
+    encodeURI 이거 붙여야 한글됨
+    */
 }
 
 export default login
