d988150ef82655d2bd1339792ec7ba9caf821574
hanul-cha <ckgks3014@naver.com>
2021-12-21 21:28:18 +0900

    커스텀훅

---
+++ components/customhooks/UseGraphql.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+
+
+const UseGraphql = (id :string) => {
+    const GET_USER_INFO = gql`
+    query MyQuery($userId: String!) {
+      userByUserId(userId: $userId) {
+        password
+        name
+      }
+    }
+  `;
+  
+
+  const { loading, data } = useQuery(GET_USER_INFO, {
+    variables: {
+      userId: id,
+    },
+  });
+
+    return {
+        loading,
+        data
+    }
+}
+
+export default UseGraphql
+
+//그래프 큐엘에 쿼리보내는 커스텀 훅
