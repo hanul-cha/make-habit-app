2f64eae1f84aab89a36148ec579c54714efd9306
hanul-cha <ckgks3014@naver.com>
2021-12-23 16:27:42 +0900

    db정리좀 해야되겠어

---
+++ components/customhooks/UseMutation.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const UseGraphql = (id: string) => {
+  const GET_USER_INFO = gql`
+    query MyQuery($userId: String!) {
+      userByUserId(userId: $userId) {
+        userId
+        password
+        name
+      }
+    }
+  `;
+
+  const { loading, data } = useQuery(GET_USER_INFO, {
+    variables: {
+      userId: id,
+    },
+  });
+
+  return {
+    loading,
+    data,
+  };
+};
+
+export default UseGraphql;
+
+//뮤테이션을 관리하는 커스텀 훅
--- habitDB.sql
+++ habitDB.sql
@@ -9,18 +9,19 @@ CREATE TABLE "user" (
 CREATE TABLE "myhabit" (
   "habit_id" SERIAL,
   "user_id" TEXT NOT NULL,
-  "habit_day" DATE NOT NULL,
   "habit_title" TEXT NOT NULL,
   "habit_text" TEXT NOT NULL,
-  "habit_date" INT NOT NULL,
-  "check_habit" BOOLEAN DEFAULT FALSE,
-  PRIMARY KEY ("habit_id")
+  "habit_week" INT NOT NULL,
+  PRIMARY KEY ("habit_id"),
+  CONSTRAINT "FK_myhabit.user_id"
+    FOREIGN KEY ("user_id")
+      REFERENCES "user"("user_id")
 );
 
 CREATE TABLE "todolist" (
   "todo_id" SERIAL,
   "user_id" TEXT NOT NULL,
-  "todo_day" DATE NOT NULL,
+  "todo_date" DATE NOT NULL,
   "todo_title" TEXT NOT NULL,
   "todo_text" TEXT NOT NULL,
   PRIMARY KEY ("todo_id")
@@ -32,5 +33,21 @@ INSERT INTO "user" (
 	'ccchhh1234',
 	'차한울',
 	'123456'
+);
+INSERT INTO "myhabit" (
+	user_id, habit_title, habit_text, habit_week
+) VALUES (
+	'ccchhh1234',
+	'오늘은꼭',
+	'매주 이날은 8시에 드라마를 보는날입니다.',
+  3
+);
+INSERT INTO "myhabit" (
+	user_id, habit_title, habit_text, habit_week
+) VALUES (
+	'ccchhh1234',
+	'운동하는날',
+	'6시까지 센터로 달려가!',
+  5
 );
 
