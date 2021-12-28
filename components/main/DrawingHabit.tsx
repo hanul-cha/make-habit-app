bb348eef3a109c761007d7ebd0ad5516ab2801b4
hanul-cha <ckgks3014@naver.com>
2021-12-28 16:57:44 +0900

    이제 진짜 css좀 건들여야 할듯 너무 안이뻐

--- components/main/DrawingHabit.tsx
+++ components/main/DrawingHabit.tsx
@@ -1,4 +1,17 @@
-import React from "react";
+import * as React from "react";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import SendIcon from "@mui/icons-material/Send";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import StarBorder from "@mui/icons-material/StarBorder";
+import DoubleArrowRoundedIcon from "@mui/icons-material/DoubleArrowRounded";
 
 interface DrawingHabitType {
   e?: {
@@ -8,12 +21,32 @@ interface DrawingHabitType {
 }
 
 const DrawingHabit = ({ e }: DrawingHabitType) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <>
       {e && (
-        <div>
-          <h3>{e.node.habitTitle}</h3>
-          <p>{e.node.habitText}</p>
+          <div className="drawDiv">
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
+        >
+          <ListItemButton onClick={handleClick}>
+            <ListItemText primary={e.node.habitTitle} />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary={e.node.habitText} />
+              </ListItemButton>
+            </List>
+          </Collapse>
+        </List>
         </div>
       )}
     </>
