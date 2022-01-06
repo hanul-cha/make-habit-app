5588735ba26a865d81cbc08e541bf6107d0226e3
hanul-cha <ckgks3014@naver.com>
2022-01-06 12:44:46 +0900

    컴포넌트 한개는 더 생길거같음

--- components/addhabit/AddhabitMain.tsx
+++ components/addhabit/AddhabitMain.tsx
@@ -1,28 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import PleaseLogin from '../main/PleaseLogin';
+import PleaseLogin from "../main/PleaseLogin";
+import { useQuery, gql } from "@apollo/client";
+import HobbiesByDay from "./HobbiesByDay";
+
+const GET_WEEK_HABIT = gql`
+  query MyQuery($userInfo: String!) {
+    userByUserId(userId: $userInfo) {
+      myhabitsByUserId {
+        nodes {
+          habitId
+          habitWeek
+        }
+      }
+      name
+    }
+  }
+`;
 
 const AddhabitMain = () => {
-    const [userInfo, setUserInfo] = useState();
+  const [userInfo, setUserInfo] = useState();
 
-    useEffect(() => {
-        axios.get("/api/isLogin").then((res) => {
-            if (res.status === 200 && res.data.id) {
-                setUserInfo(res.data.id);
-            }
-          });
-    })
+  useEffect(() => {
+    axios.get("/api/isLogin").then((res) => {
+      if (res.status === 200 && res.data.id) {
+        setUserInfo(res.data.id);
+      }
+    });
+  });
 
+  const { data, loading } = useQuery(GET_WEEK_HABIT, {
+    variables: {
+      userInfo,
+    },
+  });
 
-    return (
-        <>
-            {userInfo !== undefined? (
-                ""
-            ):(
-                <PleaseLogin />              
-            )}
-        </>
-    )
-}
+  console.log(data?.userByUserId?.name);
 
-export default AddhabitMain
+  return <>
+  {userInfo !== undefined ? 
+  <div>
+      <h2>{data?.userByUserId?.name}님의 취미</h2>
+      <HobbiesByDay />
+  </div> 
+  : 
+  <PleaseLogin />}</>;
+};
+
+export default AddhabitMain;
