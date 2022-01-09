abe544cc94a576b2d419d76dae0a64fe63f8d909
hanul-cha <ckgks3014@naver.com>
2022-01-09 10:11:04 +0900

    css수정

--- components/addhabit/HobbiesByDay.tsx
+++ components/addhabit/HobbiesByDay.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import Collapse from "@mui/material/Collapse";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
 
 interface nodeType {
   habitId: number;
@@ -19,8 +13,6 @@ interface HobbiesByDayType {
 
 const HobbiesByDay = ({ node }: HobbiesByDayType) => {
   console.log(node);
-
-  const [open, setOpen] = React.useState(false); //클릭여부를 저장하는 state
 
   const runfilter = (day: number) => {
     if (node !== undefined) {
@@ -58,51 +50,64 @@ const HobbiesByDay = ({ node }: HobbiesByDayType) => {
       list: runfilter(6),
     },
   ]; //요일별로 들어온 요일인덱스와 runfilter함수에서 리턴한 조건에 맞는 리스트가 들어있음
-  console.log(hobbiesByDayList);
+  /* console.log(hobbiesByDayList); */
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const trueHeight = e.currentTarget.nextElementSibling?.firstElementChild?.clientHeight
-    e.currentTarget.nextElementSibling?.animate([{ height: `${trueHeight}px` }], {
-      duration: 50,
-      fill: "forwards",
-    });
+    const trueHeight =
+      e.currentTarget.nextElementSibling?.firstElementChild?.clientHeight;
+    if (e.currentTarget.nextElementSibling?.clientHeight == 0) {
+      //졉혀있다면
+      e.currentTarget.nextElementSibling?.animate(
+        [{ height: `${trueHeight}px` }],
+        {
+          duration: 50,
+          fill: "forwards",
+        }
+      );
+    } else {
+      e.currentTarget.nextElementSibling?.animate([{ height: 0 }], {
+        //펼처져 있다면
+        duration: 50,
+        fill: "forwards",
+      });
+    }
     //animate속성에 픽셀을 지정할땐 특정한 값을 입력해줘야한다. auto안됨 그래서 inner컨테이너를 만들어 높이를 변수에
-    //저장하고 그 높이로 
+    //저장하고 그 높이로 지정해주었음
   }; //클릭하면 밑으로 리스트를 보여줄것임
-
-  /* 
-  habitList의 list길이를 사용하면 요일별 취미개수를 얻을수 있다 
-  현재 문제는 위에 헨들클릭함수가 map으로 생성된 모든 하위태그에서 작동하기때문에 내가 직접 만들어야 할듯함...
-  */
 
   return (
     <div className="HobbiesByDay">
       <h2>요일별 취미</h2>
-      <List
-        sx={{ width: "100%", bgcolor: "background.paper" }}
-        component="nav"
-        aria-labelledby="nested-list-subheader"
-      >
+      <div className="HobbiesByDayMainCon">
         <>
           {hobbiesByDayList.map((day, i) => {
-            console.log(day);
+            console.log(day.list);
             return (
-              <div key={i}>
-                <ListItemButton onClick={(e) => handleClick(e)}>
-                  <ListItemText className="" primary={day.day} />
-                  {/* 취미제목 */}
-                  {open ? <ExpandLess /> : <ExpandMore />}
-                </ListItemButton>
+              <div className="HobbiesByDayList" key={i}>
+                <div onClick={(e) => handleClick(e)}>
+                  <h2>{day.day}</h2>
+                  <h3>취미 개수 : {day.list?.length}</h3>
+                </div>
                 <div className="habbieByDayCustomList">
                   <div className="customListInner">
-                    <h3>내용</h3>
+                    {day.list?.length !== 0 ? (
+                      <div className="haveHobbie">
+                        {day.list?.map((hobbies, i) => {
+                          return <div key={i}>{hobbies.habitTitle}</div>;
+                        })}
+                      </div>
+                    ) : (
+                      <div className="tryAddBabit">
+                        해당 요일엔 등록된 취미가 없습니다.
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
             );
           })}
         </>
-      </List>
+      </div>
     </div>
   );
 };
