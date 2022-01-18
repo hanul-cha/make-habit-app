a2a30558aece704c9383461d4797c614922440fd
hanul-cha <ckgks3014@naver.com>
2022-01-18 16:31:07 +0900

    쿠키셋팅에 이름 지정

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const logout = async (req: any, res:NextApiResponse) => {
-        res.setHeader("Set-Cookie",`id=1234;Max-Age=0;HttpOnly,Secure`)
+        res.setHeader("Set-Cookie",`id=${req.cookies.id};Max-Age=0;HttpOnly,Secure`)
         res.statusCode = 200;
         res.json({message: "ok"})
 }
