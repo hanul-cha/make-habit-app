a996f86eb12961e322d47ac58be798cd4a9fcce2
hanul-cha <ckgks3014@naver.com>
2022-01-18 16:43:57 +0900

    로그아웃 테스트

--- pages/api/isLogin.ts
+++ pages/api/isLogin.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const isLogin = (req:NextApiRequest, res:NextApiResponse) => {
+    console.log(req.cookies.id, "isLogin")
     res.statusCode = 200;
     res.json({ id : req.cookies.id });
 }
